11e9caedaa3ec4b9f9562aca18493085489ba57c
Matthew Lyon <2654239l@student.gla.ac.uk>
2023-03-02 22:36:04 +0000

    Initial Setup

--- wad2groupproject/settings.py
+++ wad2groupproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'com^$k$_2wkz$@s%_b!x125j88qc816i3f5otufe!-x9law#_%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thepetproject.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'thepetproject',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'wad2groupproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- wad2groupproject/urls.py
+++ wad2groupproject/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from thepetproject import views
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('thepetproject/', include('thepetproject.urls')),
     path('', views.index, name="index"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
