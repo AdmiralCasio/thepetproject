0e5bef1d68d0159494ecd36ec7d1a6566e28ca0e
2631613a <2631613a@student.gla.ac.uk>
2023-03-05 17:28:29 +0000

    created homepage view and started template

--- thepetproject/views.py
+++ thepetproject/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from thepetproject.models import Post
 
 def index(request):
-    return render(request, 'thepetproject/index.html')
+    post_list = Post.objects.order_by('-likes')[:3]
+    context_dict = {}
+    context_dict['posts'] = post_list
+    return render(request, 'thepetproject/index.html', context=context_dict)
 
