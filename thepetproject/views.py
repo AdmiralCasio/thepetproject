01eaf3ccddd680b9c952a8fd76eb7a9ebc93b543
Sam Parker <2620865P@student.gla.ac.uk>
2023-03-07 09:40:15 +0000

    Updated template to handle formatting on page load and added JS to format buttons after user likes a post/comment

--- thepetproject/views.py
+++ thepetproject/views.py
@@ -9,7 +9,21 @@ def view_individual_post(request, post_id):
     post = Post.objects.get(post_id)
     comment = Comment.objects.order_by('-date').order_by('-time')[0]
     current_user = request.user
-    context_dict = {'post': post, 'comment': comment, 'user': current_user}
+    try:
+        has_user_liked_post = UserHasLikedPost.get(current_user.username)
+    except:
+        has_user_liked_post = False
+    else:
+        has_user_liked_post = True
+    try:
+        has_user_liked_comment = UserHasLikedComment.get(current_user.username)
+    except:
+        has_user_liked_comment = False
+    else:
+        has_user_liked_comment = True
+    context_dict = {'post': post, 'comment': comment, 'user': current_user,
+                    'has_user_liked_post': has_user_liked_post,
+                    'has_user_liked_comment': has_user_liked_comment}
 
     return render(request, 'thepetproject/view_individual_post.html', context_dict)
 
